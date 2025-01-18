3c706b94603f843bf96cd527699d556d25eaed39
Alberto6126 <alberto.sanchez@enti.cat>
2025-01-18 19:03:44 +0100

    JavaScript contacto actualizado

--- scriptcontacto.js
+++ scriptcontacto.js
@@ -1,8 +1,30 @@
-
+let nombre = "";
+let fecha = "";
+let contacto = "";
+let telefono = "";
+let color = "";
+let mensaje = "";
 
 
 function prueba(evento){
     evento.preventDefault();
+    nombre = document.getElementById("nombreForm");
+    if (document.getElementById(telefonoForm).value.length > 9 && document.getElementById(fechaForm).value.length < 20){
+        telefono = document.getElementById("telefonoForm");
+    }
+    contacto = document.getElementById("emailForm");
+    telefono = document.getElementById("");
+    color = document.getElementById("");
+    mensaje = document.getElementById("");
     console.log("Procesando envio");
+    console.log(nombre.value);
+    if(nombre.value.length < 2){
+        console.log("El nombre tiene que ser mayor a 1 caracter")
+    }
+    if(contacto.value < 5){
+        console,log("El correo no es correcto")
+    }
+
+
 }
 
