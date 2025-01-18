6ad6427b52588ebb8b57b5df06777316167958ab
Alberto6126 <alberto.sanchez@enti.cat>
2025-01-18 18:38:38 +0100

    Deberes pizzaluna

--- scriptcontacto.js
+++ scriptcontacto.js
@@ -1,29 +1,13 @@
-let nombre = "";
-let fecha ="";
-let email = "";
-let telefono ="";
-let color ="";
-let mensaje="";
+let nombre = document.getElementById;
+let fecha = document.getElementById;
+let contacto = document.getElementById;
+let telefono = document.getElementById;
+let color = document.getElementById;
+let mensaje = document.getElementById;
 
-function asignacionVariables(){
-nombre = document.getElementById("nombreForm").value;
-email = document.getElementById("emailForm").value;
-
-if(document.getElementById("telefonoForm").value != ""){
-    telefono = document.getElementById("telefonoForm").value;
-    console.log(telefono)
-}
-if(document.getElementById("fechaForm").value != ""){
-    fecha = document.getElementById("fechaForm").value;
-    console.log(fecha)
-}
-if(document.getElementById("colorForm").value != ""){
-    color = document.getElementById("colorForm").value;
-    console.log(color)
-}
-if(document.getElementById("mensajeForm").value != ""){
-    mensaje = document.getElementById("mensajeForm").value;
-    console.log(mensaje)
-}
-
+function envioForm(evento){
+    evento.preventDefault();
+    if(nombre < 2){
+        console.log("polla");
+    }
 }
